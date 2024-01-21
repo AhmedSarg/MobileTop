da68360502537c3a0c2e1729d59e0a9430592256
AhmedSarg <xsarg22@gmail.com>
2024-01-21 06:48:45 +0200

    test

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<html>
-  <body>
-    test test test
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  the php site
+</body>
 </html>
